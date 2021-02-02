48a54b7c5690ebb5e73c3664132b0488170ec0b9
cflensbatina <65843576+cflensbatina@users.noreply.github.com>
2021-02-02 13:07:58 -0500

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,6 @@
+# Keep this file separate
+
+# Gmail for auto emails
+def auto_email():
+    return {"user_name": "user",
+            "password": "password1"}
---
+++ test email login.py
@@ -0,0 +1,5 @@
+import hidden
+
+# Use password file
+user_name = hidden.auto_email()["user_name"]
+password = hidden.auto_email()["password"]
